c85a2f62ebb136fc70f8a836292fa874f20db2d6
YadneshGawas <yadnesh.gawas@gmail.com>
2024-07-23 12:06:07 +0530

    Update TaskDialog.jsx
    Default date is working

--- client/src/other/task/TaskDialog.jsx
+++ client/src/other/task/TaskDialog.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { Fragment, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { AiTwotoneFolderOpen } from "react-icons/ai";
 import { BsThreeDots } from "react-icons/bs";
 import { HiDuplicate } from "react-icons/hi";
@@ -13,9 +13,7 @@ import ConfirmatioDialog from "../Dialogs";
 import AddSubTask from "./AddSubTask";
 import { useSelector } from "react-redux";
 
-
 const TaskDialog = ({ task }) => {
-
   const { user } = useSelector((state) => state.auth);
   const [open, setOpen] = useState(false);
   const [openEdit, setOpenEdit] = useState(false);
@@ -30,46 +28,40 @@ const TaskDialog = ({ task }) => {
 
   const items = [
     {
-      label: user.isAdmin ? "Open Project" : "Open Task",
-      icon: <AiTwotoneFolderOpen className='mr-2 h-5 w-5' aria-hidden='true' />,
-      onClick: () => navigate(`/projects/${projectId}/tasks`),//to be modified
+      label: "Open",
+      icon: <AiTwotoneFolderOpen className="mr-2 h-5 w-5" aria-hidden="true" />,
+      onClick: () =>
+        navigate(
+          user.isAdmin ? `/project/${projectId}/tasks` : `/task/${task._id}`
+        ), //true : fasle
+      // onClick: () => navigate(`/projects/${projectId}/tasks`),
     },
     {
       label: "Edit",
-      icon: <MdOutlineEdit className='mr-2 h-5 w-5' aria-hidden='true' />,
+      icon: <MdOutlineEdit className="mr-2 h-5 w-5" aria-hidden="true" />,
       onClick: () => setOpenEdit(true),
-    },
-    {
-      label: user.isAdmin ? "Add Task" : "Add Sub-Task",
-      icon: <MdAdd className='mr-2 h-5 w-5' aria-hidden='true' />,
-      onClick: () => setOpen(true),
-    },
-    {
-      label: "Duplicate",
-      icon: <HiDuplicate className='mr-2 h-5 w-5' aria-hidden='true' />,
-      onClick: () => duplicateHandler(),
     },
   ];
 
   return (
     <>
       <div>
-        <Menu as='div' className='relative inline-block text-left'>
-          <Menu.Button className='inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-gray-600 '>
+        <Menu as="div" className="relative inline-block text-left">
+          <Menu.Button className="inline-flex w-full justify-center rounded-md px-4 py-2 text-sm font-medium text-gray-600 ">
             <BsThreeDots />
           </Menu.Button>
 
           <Transition
             as={Fragment}
-            enter='transition ease-out duration-100'
-            enterFrom='transform opacity-0 scale-95'
-            enterTo='transform opacity-100 scale-100'
-            leave='transition ease-in duration-75'
-            leaveFrom='transform opacity-100 scale-100'
-            leaveTo='transform opacity-0 scale-95'
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
           >
-            <Menu.Items className='absolute p-4 right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none'>
-              <div className='px-1 py-1 space-y-2'>
+            <Menu.Items className="absolute p-4 right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+              <div className="px-1 py-1 space-y-2">
                 {items.map((el) => (
                   <Menu.Item key={el.label}>
                     {({ active }) => (
@@ -86,25 +78,26 @@ const TaskDialog = ({ task }) => {
                   </Menu.Item>
                 ))}
               </div>
-
-              <div className='px-1 py-1'>
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      onClick={() => deleteClicks()}
-                      className={`${
-                        active ? "bg-blue-500 text-white" : "text-red-900"
-                      } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                    >
-                      <RiDeleteBin6Line
-                        className='mr-2 h-5 w-5 text-red-400'
-                        aria-hidden='true'
-                      />
-                      Delete
-                    </button>
-                  )}
-                </Menu.Item>
-              </div>
+              {user.isAdmin && (
+                <div className="px-1 py-1">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <button
+                        onClick={() => deleteClicks()}
+                        className={`${
+                          active ? "bg-blue-500 text-white" : "text-red-900"
+                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                      >
+                        <RiDeleteBin6Line
+                          className="mr-2 h-5 w-5 text-red-400"
+                          aria-hidden="true"
+                        />
+                        Delete
+                      </button>
+                    )}
+                  </Menu.Item>
+                </div>
+              )}
             </Menu.Items>
           </Transition>
         </Menu>
